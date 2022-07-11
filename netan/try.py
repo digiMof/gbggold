6d0f57f7c1ab1c85423842adc1cc25e0b9ea31c7
Orsola Maria Borrini <orsolaborrini@gmail.com>
2022-07-12 00:19:59 +0200

    reg ex are a mess

--- netan/try.py
+++ netan/try.py
@@ -1,15 +1,7 @@
 import re
 import csv
 
-# Clean the newly .html 
-    # Remove all <p> tags
-    # Remove all </p> tags
-    # Remove all <h5> tags
-    # Remove all </h5> tags
-    # Remove all the html-related tags (<head></head>, <body></body>)
-
-# Since we want to distinguish between the two networks of Fadigati before and after the discovery of his sexuality, we need to split the full text into two different corpora (the before and after corpora)
-
+# Since we want to distinguish between the two networks of Fadigati before and after the discovery of his sexuality, we need to split the full text into two different corpora (the before and after corpora): two txt files (NO tags except for the pers-name and place-name)
 
 # Starting corpora -> corpora1.txt, corpora2.txt
 # Open the two corpora and retrieve the text as a string
@@ -25,20 +17,28 @@ corpora_1.replace("\n"," ")
 corpora_2.replace("\n"," ")
 #print("STRIP FROM NEWLINE",corpora_1)
 
-# Split corpora_1 in sentences (use the period .) --> # Now you will have a list of sentences
-#txt_1 = nltk.tokenize.sent_tokenize(corpora_1)
+# Split the corpora in sentences (using multiple delimiters with regular expressions) --> # Now you will have a list of sentences/strings
+txt_1 = re.split("\.\s|\.\n|\?\s|\?\n|\!\s|!\n",corpora_1)
+    # Maybe the actual sentence-ending period would be this one \.\s+[A-Z]
+txt_2 = re.split("\.\s|\.\n|\?\s|\?\n|\!\s|!\n",corpora_2)
 
-#Split list based on multiple delimiters
-txt_1 = re.split("\."|"?"|"!",corpora_1)
-print("Divided text\n",txt_1)
+print(txt_1)
 
-# txt_2 = nltk.tokenize.sent_tokenize(corpora_2)
-
-# Create an empty list for the tuples that I will need for the csv
-tpl_list1 = list()
-for item in txt_1:
-    tpl = tuple()
-
+tpl_list1 = list()  # Create an empty list for the tuples that I will need for the csv (2 columns: source (node) and target (node))
+for item in txt_1:  # We scroll through the list of sentences(:str)
+    print("ITEM\n",item)
+    counter = 0     
+    words = item.split("\s[^\>\w*\s?\w*\<]")  # We split the sentence in words -> words is a list of strings
+    # Here we cannot use just the blank space cause otherwise it will split in two different sentences also the name inside the pers-name tag, which is the one we are interested in!
+    print("This is the list of strings\n",words)
+    '''
+    for wrd in words:        # For every word in the list of words
+        if "pers-name" in wrd:  # If that word contains "pers-name" THEN it IS a character!
+            counter += 1
+            name_char = re.match("\>(.*)\<",wrd)    #\s?("\>\w*\s?\w*\<?)\s? -> this is the correct one, it will match whatever is inside the tags (just remember to clean it removing the <> afterwards (NOT BEFORE because they are important characters to identify this specific part of the strings))
+            print(name_char)
+            tpl = ()
+'''
 # Iterate over the txt_1, list of sentences of corpora_1
 
 
